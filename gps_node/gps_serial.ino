04cfd28df22f89beb7c08c2044de9d96113c8f60
Herick DallAgnol <140270456+HerickDallagnol@users.noreply.github.com>
2023-08-28 19:50:44 -0300

    Add files via upload

---
+++ gps_node/gps_serial.ino
@@ -0,0 +1,56 @@
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+#include <ros.h>
+#include <sensor_msgs/NavSatFix.h>
+
+//declara os pinos da esp
+#define RXD2 16
+#define TXD2 17
+
+//define a banda do gps e cria a softwareSerial
+int GPSBaud = 9600;
+TinyGPSPlus gps;
+SoftwareSerial gpsSerial(RXD2, TXD2);
+
+//inicia o nodo e um publisher com o topico gps_data
+ros::NodeHandle nh;
+sensor_msgs::NavSatFix gps_msg;
+ros::Publisher gps_pub("gps_data", &gps_msg);
+
+void setup() {
+  Serial.begin(9600); 
+  gpsSerial.begin(GPSBaud);
+  nh.initNode();
+  nh.advertise(gps_pub);
+}
+
+void loop() {
+  while (gpsSerial.available() > 0)
+    if (gps.encode(gpsSerial.read()))
+      displayInfo();
+  if (millis() > 5000 && gps.charsProcessed() < 10) {
+    Serial.println("Sinal GPS não detectado");
+    while (true)  
+      ;
+  }
+}
+
+void displayInfo() {
+  if (gps.location.isValid()) {
+
+    rospy.info("Latitude: ")
+    gps_msg.latitude = gps.location.lat();
+    rospy.info(gps.location.lat(), 6);
+
+    rospy.info("Longitude: ");
+    gps_msg.longitude = gps.location.lng();
+    rospy.info(gps.location.lng(), 6);
+    
+    gps_pub.publish(&gps_msg);
+    nh.spinOnce();
+
+  } else {
+    rospy.info("Sem localização");
+  }
+  
+}
