408a8d043ada1a84a68004b3eea2de2ed106531d
Herick DallAgnol <140270456+HerickDallagnol@users.noreply.github.com>
2024-07-24 08:30:15 -0300

    add mag_declination
    mag_declination = 16,08
    angulo = angulo - mag_declination

--- sensors_nodes/compass_node.ino
+++ sensors_nodes/compass_node.ino
@@ -1,20 +1,21 @@
 #include <Wire.h> 
 #include <MechaQMC5883.h> 
 #include <ros.h>
-#include <std_msgs/String.h>
+#include <std_msgs/Float32.h>
+#include <math.h>
 
-MechaQMC5883 bussola; // Criacao do objeto para o sensor 
+MechaQMC5883 bussola; // Criação do objeto para o sensor 
 
 int x = 0, y = 0, z = 0;
-int angulo = 0;
+float angulo = 0;
+float mag_declination = 16.03;
 
-std_msgs::String compass_msg;
+std_msgs::Float32 compass_msg;
 ros::Publisher compass_pub("compass_data", &compass_msg);
 
 ros::NodeHandle nh;
 
-void setup()
-{
+void setup() {
   nh.initNode();
   nh.getHardware()->setBaud(9600);
   Wire.begin(); 
@@ -25,43 +26,19 @@ void setup()
   nh.advertise(compass_pub);
 }
  
-void loop() 
-{ 
+void loop() { 
   bussola.read(&x, &y, &z); 
-  angulo = atan2(x, y) / 0.0174532925; 
- 
-  // Ajuste do angulo entre 0 e 360 graus
-  if (angulo < 0) 
-    angulo += 360;
-  
-  angulo = 360 - angulo;
-  String direction = getCompassDirection(angulo);
+  angulo = atan2(x, y) / (180.0/M_PI); // Converte radianos para graus
+  angulo = angulo - mag_declination;
 
-  compass_msg.data = direction.c_str();
+  if(angulo < 0){
+    angulo += 360;
+  }
+ 
+  compass_msg.data = angulo;
   compass_pub.publish(&compass_msg);
 
   nh.spinOnce();
   
   delay(500);
-}
-
-String getCompassDirection(int angle) {
-  if (angle > 338 || angle < 22) {
-    return "Norte";
-  } else if (angle > 22 && angle < 68) {
-    return "Nordeste";
-  } else if (angle > 68 && angle < 113) {
-    return "Leste";
-  } else if (angle > 113 && angle < 158) {
-    return "Suldeste";
-  } else if (angle > 158 && angle < 203) {
-    return "Sul";
-  } else if (angle > 203 && angle < 248) {
-    return "Suldeste";
-  } else if (angle > 248 && angle < 293) {
-    return "Oeste";
-  } else if (angle > 293 && angle < 338) {
-    return "Noroeste";
-  }
-  return "Desconhecido";
 }
