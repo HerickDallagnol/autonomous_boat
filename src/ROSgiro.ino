e4acdeee93353f738fd5c955049f536178d0872f
Herick DallAgnol <140270456+HerickDallagnol@users.noreply.github.com>
2024-08-07 13:15:26 -0300

    add north_msg

--- src/ROSgiro.ino
+++ src/ROSgiro.ino
@@ -2,16 +2,21 @@
 #include <MechaQMC5883.h> 
 #include <ros.h>
 #include <std_msgs/Float32.h>
+#include <std_msgs/Bool.h>
 #include <math.h>
 
 MechaQMC5883 bussola;
 
 int x = 0, y = 0, z = 0;
 float angulo = 0;
+float angulo1 = 0;
 float mag_declination = 16.03;
 
 std_msgs::Float32 compass_msg;
 ros::Publisher compass_pub("compass_data", &compass_msg);
+
+std_msgs::Bool north_msg;
+ros::Publisher north_pub("north_detected", &north_msg);
 
 ros::NodeHandle nh;
 
@@ -24,19 +29,29 @@ void setup() {
   bussola.init();
   
   nh.advertise(compass_pub);
+  nh.advertise(north_pub);
 }
- 
+
 void loop() { 
   bussola.read(&x, &y, &z); 
-  angulo = atan2(x, y) / (180.0/M_PI);
-  angulo = angulo - mag_declination;
+  angulo = atan2(x, y) / (180.0 / M_PI);
+  angulo1 = angulo - mag_declination;
 
-  if(angulo < 0){
+  if (angulo1 < 0) {
+    angulo1 += 360;
+  }
+  compass_msg.data = angulo1;
+  compass_pub.publish(&compass_msg);
+
+  if (angulo < 0) {
     angulo += 360;
   }
- 
-  compass_msg.data = angulo;
-  compass_pub.publish(&compass_msg);
+  if (fabs(angulo) < 5.0) { 
+    north_msg.data = true;
+  } else {
+    north_msg.data = false;
+  }
+  north_pub.publish(&north_msg);
 
   nh.spinOnce();
   
